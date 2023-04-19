8bf684853bdfdeb67248ef14aef89a8aa6d18257
Slonexxx <21moriarty21@mail.ru>
2023-04-19 11:10:32 +0600

    new kassa

--- public/Config/config.php
+++ public/Config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'appId' => '6921af1e-4a3d-4449-bd1c-eb04d8afdd30',
-    'appUid' => 'prosklad.smartinnovations',
-    'secretKey' => '94t405PWPJDQdwwPXIyg5DQomiReULzdQFcsqxOwJO1hSOKlG8f8joVBoHMXSXfb39xSUkMdY1yMVSfKHNnL6i7CKDJ7HvANRGdYjlqZKi7T5AN6uKOV3jV2EHi7GwPs',
-    'appBaseUrl' => 'https://smartwiponkassa.kz/',
+    'appId' => '0d1c2b73-287d-4598-b406-550df40b1b35',
+    'appUid' => 'nurkassa.smartinnovations',
+    'secretKey' => 'X5Fe3VMGJQhbUpihfMlrY64dTPEwsxukXR3D3ykcnw9P3qerXE5CgOtwIChhKuMSTxwoNoSBR2ZQitRYjBohZYxfJjL1JnvMQ6AylchGzUBg6xhUSSjrD1riv5YMkDsJ',
+    'appBaseUrl' => 'https://smartnurkassa.kz/',
 ];
