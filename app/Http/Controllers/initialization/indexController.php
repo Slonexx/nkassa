5859aa2080a091c06a0af6da5b3b92a1ad8582d9
Slonexxx <21moriarty21@mail.ru>
2023-05-19 11:06:40 +0600

    new kassa

--- app/Http/Controllers/initialization/indexController.php
+++ app/Http/Controllers/initialization/indexController.php
@@ -46,4 +46,47 @@ class indexController extends Controller
 
     }
 
+    public function check(Request $request){
+
+         $tmp = file_get_contents('AGOSTINA_f032b9441637d434620392928079e253669dc970.p12');
+        $certificates = "";
+        $cert = "";
+        $pkey = "";
+        $pass = "Aa1234";
+
+        if (openssl_pkcs12_read($tmp,$certificates, $pass)){
+            $index = 0;
+            $Count = count(preg_split("/((\r?\n)|(\r\n?))/", $certificates['cert']));
+            foreach(preg_split("/((\r?\n)|(\r\n?))/", $certificates['cert']) as $line){
+                $index++;
+                if ($index == 1 or ($index > $Count-2)) {
+
+                }   else {
+                    $cert = $cert.$line;
+                }
+
+            }
+
+            $index = 0;
+            $Count = count(preg_split("/((\r?\n)|(\r\n?))/", $certificates['pkey']));
+            foreach(preg_split("/((\r?\n)|(\r\n?))/", $certificates['pkey']) as $line){
+                $index++;
+                if ($index == 1 or ($index > $Count-2)) {
+
+                }   else {
+                    $pkey = $pkey.$line;
+                }
+
+            }
+
+            //dd( $data );
+
+            dd($certificates, $cert, $pkey);
+        } else {
+            dd('no');
+        }
+
+
+    }
+
 }
