17ca7470184a894bea1dd2439ee9bf827c9e6282
Slonexxx <21moriarty21@mail.ru>
2023-10-23 12:52:16 +0600

    new adress ms

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -14,7 +14,7 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <script src ="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src ="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
+    <script type="text/javascript" src="https://apps-api.moysklad.ru/js/ns/appstore/app/v1/moysklad-iframe-expand-3.js"></script>
 
 </head>
